bda730298ed6cd79124685549c0558d7ab7695d2
epszaw <konstantin@epishev.me>
2024-06-03 15:08:11 +0200

    add codecept cjs js example

--- codeceptjs-cjs-js/codecept.conf.js
+++ codeceptjs-cjs-js/codecept.conf.js
@@ -1,7 +1,17 @@
+const { setCommonPlugins } = require("@codeceptjs/configure");
+
+setCommonPlugins();
+
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
   tests: './test/*_test.js',
   output: './output',
+  plugins: {
+    allure: {
+      require: "allure-codeceptjs",
+      enabled: true,
+    },
+  },
   helpers: {
     Playwright: {
       browser: 'chromium',
