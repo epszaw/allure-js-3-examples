a3677584abdcd951d3290473d0748f1585fb959e
epszaw <konstantin@epishev.me>
2024-06-03 17:04:14 +0200

    add cucumberjs cjs js example

---
+++ cucumberjs-cjs-javascript/.prettierrc.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  trailingComma: "all",
+  singleQuote: false,
+  arrowParens: "always",
+  printWidth: 120
+};
---
+++ cucumberjs-cjs-javascript/features/sample.feature
@@ -0,0 +1,5 @@
+	@severity:FOO
+	Feature: sample feature
+		@feature:FOO
+		Scenario: sample scenario
+			Given sample step
