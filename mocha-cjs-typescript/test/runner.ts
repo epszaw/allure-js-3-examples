53ebe402ce15b1bd145adccb7b1718373a1f80cf
epszaw <konstantin@epishev.me>
2024-06-04 12:04:38 +0200

    add mocha cjs js and ts examples

---
+++ mocha-cjs-typescript/test/runner.ts
@@ -0,0 +1,15 @@
+const path = require("path");
+const glob = require("glob");
+const Mocha = require("mocha");
+
+const mocha = new Mocha({
+	timeout: 16000,
+	reporter: "allure-mocha",
+	parallel: false,
+	reporterOptions: {
+		resultsDir: path.resolve(__dirname, "../allure-results"),
+	},
+});
+
+glob.sync("./test/spec/*.ts").forEach((file) => mocha.addFile(file));
+mocha.run((failures) => process.exit(failures === 0 ? 0 : 1));
